091ba400ca210e146ad3e6eb7032cdcd18cf74e9
Higor <higor.a.santos12@hotmail.com>
2023-11-18 18:34:26 -0300

    feat: Create user service

---
+++ src/main/java/br/com/fiap/services/UserService.java
@@ -0,0 +1,13 @@
+package br.com.fiap.services;
+
+import br.com.fiap.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+}
