f421fbfd97a90d9aa22fb6e8d24c302763da1be2
Higor <higor.a.santos12@hotmail.com>
2023-11-18 19:49:44 -0300

    feat: update UserRepository

--- src/main/java/br/com/fiap/repositories/UserRepository.java
+++ src/main/java/br/com/fiap/repositories/UserRepository.java
@@ -2,6 +2,7 @@ package br.com.fiap.repositories;
 
 import br.com.fiap.domain.User.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Optional;
 import java.util.UUID;
@@ -11,4 +12,6 @@ public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findUserByEmail(String email);
 
     Optional<User> findUserById(UUID id);
+
+    UserDetails findByEmail(String email);
 }
