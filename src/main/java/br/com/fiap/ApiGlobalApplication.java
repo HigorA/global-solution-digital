744947f8a2d449b34e1a8dcf8fbcce185cdb40ba
Higor <higor.a.santos12@hotmail.com>
2023-11-22 16:05:37 -0300

    feat: add Swagger

--- src/main/java/br/com/fiap/ApiGlobalApplication.java
+++ src/main/java/br/com/fiap/ApiGlobalApplication.java
@@ -1,9 +1,19 @@
 package br.com.fiap;
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@SecurityScheme(
+		name = "Bearer Authentication",
+		scheme = "bearer",
+		bearerFormat = "JWT",
+		type = SecuritySchemeType.HTTP
+)
+@EnableCaching
 public class ApiGlobalApplication {
 
 	public static void main(String[] args) {
